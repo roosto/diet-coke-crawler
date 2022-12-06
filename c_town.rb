0d69d87180482a7b64c2f079ab68f6420d527a30
Dustin Goldman <dustin@goldmans.net>
2022-12-05 21:59:17 -0500

    add searching and screenshotting to c_town crawler
    - crawler script is now working as POC
    - finds products matching 	`pepsi` at a single URL
    - clicks each matching item to put in in the right hand details pane
    - saves screenshot of browswer with details page

--- c_town.rb
+++ c_town.rb
@@ -3,9 +3,19 @@
 require 'capybara'
 require 'pry'
 
+PRODUCT_KEY_WORDS = 'pepsi'.freeze
+
 @session = Capybara::Session.new(:selenium_chrome)
 @session.visit('https://www.ctownsupermarkets.com/weekly-circular?store_code=U41_154&store_zip=10026&store_city=New%20York&store_state=NY')
 
-sleep 5 # wait for slow async calls to resolve
+sleep 7 # wait for slow async calls to resolve
 
-binding.pry
+flyer_iframe = @session.find('iframe[title="main panel" i]')
+@session.within_frame(flyer_iframe) do
+  @session.find_all("button[aria-label*='#{PRODUCT_KEY_WORDS}' i]", visible: :all).each_with_index do |elem, index|
+    @session.scroll_to elem
+    elem.click
+    sleep 3
+    @session.save_screenshot "c_town/test-#{index}.png"
+  end
+end
